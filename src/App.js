0a3905250f5c7ac71343949ab0c33dc9c952703d
reynerdev <rloza@pucp.pe>
2020-06-16 22:32:22 -0500

    PrimerCard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import Cart from '../src/containers/Cart'
 function App() {
   return (
+    <div>
+      <Cart/>
+    </div>
     
   );
 }
---
+++ src/containers/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductCart from '../components/ProductCart'
+
+const Cart = () => {
+    return (
+        <div>
+            <ProductCart/>
+        </div>
+    )
+}
+
+export default Cart
