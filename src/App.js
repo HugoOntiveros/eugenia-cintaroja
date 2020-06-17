9e30a61640fdbd3a55210774cc32282689bea4d6
Roberto Alcantara <ralcantara_56@mac.com>
2020-06-16 22:40:36 -0500

    Add my Form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Formulario from './containers/FormularioContainer'
+
+
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Switch>
+        <Route exact path="/Form" component={Formulario} />
         <Route exact path="/" component="" />
       </Switch>
     </BrowserRouter>
