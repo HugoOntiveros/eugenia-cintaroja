76901d38443de6c8d5b0d206224ca56478681568
RodelinDipre <rdbivieca@gmail.com>
2020-06-18 20:14:24 -0400

    Agregando cambios en el Navbar y agregando el contenedor home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
+import './containers/home.css'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './containers/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Switch>
-        <Route exact path="/" component="" />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/container-nosotros" component={"Contenedor"} />
+        <Route exact path="/" component={"Contenedor"} />
       </Switch>
     </BrowserRouter>
   );
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import pastel from '../assets/PASTEL_INICIO.png';
 
 const Home = () => {
     return (
-        <div>
-            
+
+        <div className="container">
+            <section className="fondo">
+                <div className="contenedor">
+
+                    <div className="contenedor_texto">
+                        <p className="texto_eugenia">Eugenia</p>
+                        <p className="texto_inicio">PASTELERIA <br /> CETOGENICA</p>
+                        <button type="button" className="btn " style={{ background: "#ffd600" }}
+                            data-toggle="modal"
+                            data-target="#exampleModalCenter">
+                            Pasteleria Eugenia
+                        </button>
+                    </div>
+
+                    <div>
+                        <img className="pastel" src={pastel} alt="fotopastel" />
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
